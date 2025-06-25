12b842f54e8f6fdf8b0b39d1966de3df9b1939fb
Lado <lado.asambadze1@gmail.com>
2025-06-25 18:53:55 +0400

    turnoff signup

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,6 +1,5 @@
 import express from "express"
 import { SignIn } from "../../controllers/auth/Signin"
-import { SignUp } from "../../controllers/auth/Signup"
 import { authenticateToken } from "../../middlewares/auth"
 import { SignOut } from "../../controllers/auth/LogOut"
 import { checkAuthStatus } from "../../controllers/auth/Status"
@@ -10,7 +9,7 @@ const router = express.Router()
 
  
 router.post("/signin", SignIn)
-router.post("/signup", SignUp)
+// router.post("/signup", SignUp)
 router.post("/signout", authenticateToken, SignOut)
 router.get("/status", authenticateToken, checkAuthStatus)  
 
